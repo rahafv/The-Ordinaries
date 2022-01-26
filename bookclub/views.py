b6a220af72f6c6749656735612f3a1f87cfdf205
Ala'a <alaa.alkaabi@kcl.ac.uk>
2022-01-26 17:21:21 +0000

    implement log in feature

--- bookclub/views.py
+++ bookclub/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LogInForm
+from django.contrib import messages
 
 def welcome(request):
     return render(request, 'welcome.html')
 
+def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        next = request.POST.get('next') or ''
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                redirect_url = next or 'feed'
+                return redirect(redirect_url)
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+    else:
+        next = request.GET.get('next') or ''
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form, 'next': next})
