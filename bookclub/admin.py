92a6ad2fba636c78f9241c6377b2d6e56bc2808f
Duna Alghamdi <duna_mohammed_s.alghamdi@kcl.ac.uk>
2022-01-26 23:23:33 +0000

    Add UserAdmin to admin.py

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from bookclub.models import User
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "first_name", "last_name"]
 # Register your models here.
