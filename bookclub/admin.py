c9a7d229b64e0350be138b9ceb9cf64eed7d4730
reema alsaif <reema.alsaif@kcl.ac.uk>
2022-01-28 00:17:49 +0000

    Add create club form tests

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from bookclub.models import User
+from bookclub.models import User, Club
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "first_name", "last_name"]
-# Register your models here.
+
+@admin.register(Club)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "theme", "owner", "meeting_type"]
