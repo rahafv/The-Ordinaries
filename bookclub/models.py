be411fc71be7774b08a9f7ea688659720b272aed
Duna Alghamdi <duna_mohammed_s.alghamdi@kcl.ac.uk>
2022-02-08 15:08:06 +0000

    Implement the meeting model and its associated tests

--- bookclub/models.py
+++ bookclub/models.py
@@ -231,3 +231,39 @@ class Rating(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
  
+class Meeting(models.Model):
+    """ The meeting model."""
+
+    title = models.CharField(
+        max_length=50, 
+        blank=False, 
+        null=False
+    )
+
+    club = models.ForeignKey(
+        Club, 
+        on_delete=models.CASCADE,
+        related_name='meeting'
+    )
+
+    book = models.ForeignKey(
+        Book, 
+        on_delete=models.CASCADE,
+        related_name='meeting'
+    )
+
+    time = models.DateTimeField(
+        auto_now_add=False,
+        blank=False,
+        null =False
+    )
+
+    link = models.URLField(
+        max_length=1000,
+        blank=False
+    )
+
+    notes = models.CharField(
+        max_length=500,
+        blank=True
+    )
