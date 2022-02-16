1b4059e4b50b6f8878a95a25f18b97cc0a00d318
Galeah Moquim <k19070616@kcl.ac.uk>
2022-02-16 11:29:09 +0000

    add more tests to make coverage 100

--- bookclub/tests/views/test_edit_review_view.py
+++ bookclub/tests/views/test_edit_review_view.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from bookclub.forms import EditRatingForm
 from bookclub.models import User, Club , Rating
 from bookclub.tests.helpers import LoginRedirectTester, MessageTester , MenueTestMixin
+from bookclub.views import handler404 
 
 class ReviewUpdateViewTest(TestCase, LoginRedirectTester, MessageTester,MenueTestMixin):
     """Test suite for the edit review view."""
@@ -11,14 +12,25 @@ class ReviewUpdateViewTest(TestCase, LoginRedirectTester, MessageTester,MenueTes
     fixtures = [
         'bookclub/tests/fixtures/default_rating.json',
         'bookclub/tests/fixtures/default_user.json' , 
-        'bookclub/tests/fixtures/default_book.json'
+        'bookclub/tests/fixtures/default_book.json', 
+        'bookclub/tests/fixtures/other_users.json' ,
+        'bookclub/tests/fixtures/other_ratings.json' , 
+        'bookclub/tests/fixtures/other_books.json'
     ]
 
     def setUp(self):
         self.rating = Rating.objects.get(id=1)
+        self.other_rating = Rating.objects.get(id=2)
         self.url = reverse("edit_review", kwargs={"review_id": self.rating.id})
+        self.other_url = reverse("edit_review", kwargs={"review_id": self.other_rating.id})
         self.form_input = {
             'user': 1,
+            'book':1,
+            'review': 'Great book',
+            'rating': 4.0,
+        }
+        self.other_form_input = {
+            'user': 2,
             'book':1,
             'review': 'Great book',
             'rating': 4.0,
@@ -58,7 +70,7 @@ class ReviewUpdateViewTest(TestCase, LoginRedirectTester, MessageTester,MenueTes
         response = self.client.post(self.url, self.form_input, follow=True)
         self.assertRedirects(response, target_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, "book_details.html")
-        self.assertEqual(count_ratings_before, Club.objects.count()+1)
+        self.assertEqual(count_ratings_before, Rating.objects.count())
         self.assert_success_message(response)
         self.rating.refresh_from_db()
         self.assertEqual(self.rating.user.id, 1)
@@ -66,6 +78,14 @@ class ReviewUpdateViewTest(TestCase, LoginRedirectTester, MessageTester,MenueTes
         self.assertEqual(self.rating.review, 'Great book')
         self.assertEqual(self.rating.rating, 4.0*2)
         self.assert_menu(response)
+    
+    def test_user_cannot_access_edit_review_of_other_users(self):
+        logged_in_user = self.client.login(username=self.rating.user.username, password="Password123")
+        self.assertNotEquals(logged_in_user , self.other_rating.user.username)
+        response = self.client.get(self.other_url)
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, '404_page.html')
+
 
     def test_get_profile_redirects_when_not_logged_in(self):
        self.assert_redirects_when_not_logged_in()
