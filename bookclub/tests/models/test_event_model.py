b7d3764c83abe268d404299bec8b22fb9e438b3a
Duna Alghamdi <duna_mohammed_s.alghamdi@kcl.ac.uk>
2022-02-17 12:55:34 +0000

    add fixtures and missing test

--- bookclub/tests/models/test_event_model.py
+++ bookclub/tests/models/test_event_model.py
@@ -10,7 +10,11 @@ class EventTest(TestCase):
                 'bookclub/tests/fixtures/other_club.json',
                 'bookclub/tests/fixtures/default_book.json',
                 'bookclub/tests/fixtures/default_meeting.json',
-                'bookclub/tests/fixtures/default_rating.json']
+                'bookclub/tests/fixtures/default_rating.json',
+                'bookclub/tests/fixtures/default_user_event.json',
+                'bookclub/tests/fixtures/default_club_event.json',
+                'bookclub/tests/fixtures/other_club_event.json',
+                'bookclub/tests/fixtures/other_user_event.json']
 
     def setUp(self):
         self.user = User.objects.get(id=1)
@@ -19,34 +23,15 @@ class EventTest(TestCase):
         self.book = Book.objects.get(id=1)
         self.meeting = Meeting.objects.get(id=1)
         self.rating = Rating.objects.get(id=1)
-        self.user_event = Event(
-            type_of_actor= 'U',
-            type_of_action= 'C',
-            user=self.user,
-            club= self.other_club,
-            message ="This user is alive??",
-        )
-        self.club_event = Event(
-            type_of_actor= 'C',
-            type_of_action= 'B',
-            club=self.club,
-            book= self.book,
-            message="This is an incredible club.",
-        )
-        self.other_user_event = Event(
-            type_of_actor= 'U',
-            type_of_action= 'R',
-            user=self.user,
-            rating= self.rating,
-            message ="This user is alive??",
-        )
-        self.other_club_event = Event(
-            type_of_actor= 'C',
-            type_of_action= 'M',
-            club=self.club,
-            meeting= self.meeting,
-            message="This is an incredible club.",
-        )
+        self.user_event = Event.objects.get(id=1)
+        self.club_event = Event.objects.get(id=2)
+        self.other_user_event = Event.objects.get(id=3)
+        self.other_club_event =Event.objects.get(id=4)
+
+    def test_save_method(self):
+        self.user_event.type_of_action = 'B'
+        self.user_event.book = self.book
+        self.user_event.save()
 
     def test_valid_user_event_message(self):
         try:
@@ -62,33 +47,27 @@ class EventTest(TestCase):
 
     def test_user_event_user_must_not_be_blank(self):
         self.user_event.user = None
-        with self.assertRaises(ValidationError):
-            self.user_event.full_clean()
+        self._assert_event_is_invalid(self.user_event)
            
     def test_club_event_club_must_not_be_blank(self):
         self.club_event.club= None
-        with self.assertRaises(ValidationError):
-            self.club_event.full_clean()
+        self._assert_event_is_invalid(self.club_event)
 
     def test_user_event_club_action_must_not_be_blank(self):
         self.user_event.club = None
-        with self.assertRaises(ValidationError):
-            self.user_event.full_clean()
+        self._assert_event_is_invalid(self.user_event)
 
     def test_club_event_book_action_must_not_be_blank(self):
         self.club_event.book= None
-        with self.assertRaises(ValidationError):
-            self.club_event.full_clean()
+        self._assert_event_is_invalid(self.club_event)
 
     def test_club_event_meeting_action_must_not_be_blank(self):
         self.other_club_event.meeting= None
-        with self.assertRaises(ValidationError):
-            self.other_club_event.full_clean()
+        self._assert_event_is_invalid(self.other_club_event)
 
     def test_user_event_rating_action_must_not_be_blank(self):
         self.other_user_event.rating = None
-        with self.assertRaises(ValidationError):
-            self.other_user_event.full_clean()
+        self._assert_event_is_invalid(self.other_user_event)
 
     def test_get_actor(self):
         self.assertEqual(self.user_event.get_actor(), self.user.username)
@@ -104,38 +83,41 @@ class EventTest(TestCase):
 
     def test_message_must_not_be_blank_for_user_event(self):
         self.user_event.message = ''
-        with self.assertRaises(ValidationError):
-            self.user_event.full_clean()
+        self._assert_event_is_invalid(self.user_event)
 
     def test_user_event_messgae_must_not_be_overlong(self):
         self.user_event.message = 'x' * 281
-        with self.assertRaises(ValidationError):
-            self.user_event.full_clean()
+        self._assert_event_is_invalid(self.user_event)
 
     def test_message_must_not_be_blank_for_club_event(self):
         self.club_event.message = ''
-        with self.assertRaises(ValidationError):
-            self.club_event.full_clean()
+        self._assert_event_is_invalid(self.club_event)
 
     def test_club_event_message_must_not_be_overlong(self):
         self.club_event.message = 'x' * 281
-        with self.assertRaises(ValidationError):
-            self.club_event.full_clean()
+        self._assert_event_is_invalid(self.club_event)
 
     def test_user_event_cannot_be_meeting(self):
-        self.other_user_event.type_of_action= 'M'
-        self.other_user_event.meeting = self.meeting
-        with self.assertRaises(ValidationError):
-            self.other_user_event.full_clean()
+        self.user_event.type_of_action= 'M'
+        self.user_event.meeting = self.meeting
+        self._assert_event_is_invalid(self.user_event)
 
     def test_club_event_cannot_be_rating(self):
-        self.other_club_event.type_of_action= 'R'
-        self.other_club_event.rating = self.rating
-        with self.assertRaises(ValidationError):
-            self.other_club_event.full_clean()
+        self.club_event.type_of_action= 'R'
+        self.club_event.rating = self.rating
+        self._assert_event_is_invalid(self.club_event)
 
     def test_club_event_cannot_be_club(self):
-        self.other_club_event.type_of_action= 'C'
-        self.other_club_event.club = self.other_club
+        self.club_event.type_of_action= 'C'
+        self.club_event.club = self.other_club
+        self._assert_event_is_invalid(self.club_event)
+
+    def _assert_event_is_valid(self, event):
+        try:
+            event.full_clean()
+        except (ValidationError):
+            self.fail('Test rating should be valid')
+
+    def _assert_event_is_invalid(self, event):
         with self.assertRaises(ValidationError):
-            self.other_club_event.full_clean()
+            event.full_clean()
