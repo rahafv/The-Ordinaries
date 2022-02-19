c570f2b488c98b173696efba0d64540d88db5f32
Galeah Moquim <k19070616@kcl.ac.uk>
2022-02-19 09:31:25 +0000

    add some tests and fix the old ones

---
+++ bookclub/tests/views/test_add_book_from_initial_list_to_my_list.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.urls import reverse
+from bookclub.models import User, Book
+from bookclub.tests.helpers import LoginRedirectTester
+
+class AddBookFromInitialListToListViewTestCase(TestCase, LoginRedirectTester):
+    
+    fixtures = ['bookclub/tests/fixtures/default_user.json',
+        'bookclub/tests/fixtures/default_book.json']
+
+    def setUp(self):
+        self.user = User.objects.get(id=1)
+        self.book = Book.objects.get(ISBN='0195153448')
+        self.url = reverse('add_book_from_initial_list_to_my_list' ,  kwargs={'book_id': self.book.id})
+
+    def test_book_details_url(self):
+        self.assertEqual(self.url, f'/initial_book_list/{self.book.id}/add_book_from_initial_list_to_my_list/')
+
+    # def test_successful_book_addition(self):
+    #     count = self.book.readers_count()
+    #     self.client.login(username=self.user.username, password='Password123')
+    #     response = self.client.get(self.url, follow=True)
+    #     target_url = reverse("book_details", kwargs={"book_id": self.book.id})
+    #     self.assertTemplateUsed(response, 'book_details.html')
+    #     self.assertRedirects(response, target_url, status_code=302, target_status_code=200)
+    #     self.assert_success_message(response)
+    #     self.assert_menu(response)
+    #     self.assertEqual(self.book.readers_count(), count+1)
+
+    # def test_successful_book_removal(self):
+    #     self.client.login(username=self.user.username, password='Password123')
+    #     self.book.add_reader(self.user)
+    #     count = self.book.readers_count()
+    #     response = self.client.get(self.url, follow=True)
+    #     target_url = reverse("book_details", kwargs={"book_id": self.book.id})
+    #     self.assertTemplateUsed(response, 'book_details.html')
+    #     self.assertRedirects(response, target_url, status_code=302, target_status_code=200)
+    #     self.assert_success_message(response)
+    #     self.assert_menu(response)
+    #     self.assertEqual(self.book.readers_count(), count-1)
+
+
+    def test_add_to_list_redirects_when_not_logged_in(self):
+        self.assert_redirects_when_not_logged_in()
+
+
+
---
+++ bookclub/tests/views/test_initial_book_list.py
@@ -0,0 +1,39 @@
+"""Tests of the initial book list view."""
+from django.test import TestCase
+from django.urls import reverse
+from bookclub.models import User
+from bookclub.tests.helpers import LoginRedirectTester
+
+
+class InitialBookListViewTestCase(TestCase, LoginRedirectTester ):
+
+    fixtures = ['bookclub/tests/fixtures/other_users.json' , 
+                'bookclub/tests/fixtures/other_books.json'
+    ]
+
+    def setUp(self):
+        self.url = reverse('initial_book_list')
+        self.user = User.objects.get(id=5)
+        self.other_user = User.objects.get(id=3)
+
+    def test_initial_book_list_url(self):
+        self.assertEqual(self.url,'/initial_book_list/') 
+
+    # def test_continue_button_enabled(self):
+    #      self.client.login(username=self.other_user.username, password='Password123')
+           #self.assertTemplateUsed(response, 'initial_book_list.html')
+   
+
+    # def test_show_only_first_eight_books(self):
+    #      self.client.login(username=self.user.username, password='Password123')
+    #      response = self.client.get(self.url)
+    #      self.assertTemplateUsed(response, 'initial_book_list.html')
+    #      number_of_books = 
+    #      self.assertEqual()
+
+
+    def test_initial_book_list_when_not_logged_in(self):
+        self.assert_redirects_when_not_logged_in()
+
+
+
