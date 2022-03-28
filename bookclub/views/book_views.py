6754d86e062b16be1fb449af34cff2db4fc6eb96
Ala'a <alaa.alkaabi@kcl.ac.uk>
2022-03-28 16:37:46 +0100

    add views files

--- bookclub/views/book_views.py
+++ bookclub/views/book_views.py
@@ -14,6 +14,268 @@ from django.urls import reverse_lazy
 
 class AddBookView(FormView):
     form_class = BookForm
+    template_name = 'add_book.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        self.book = form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('book_details', kwargs = {'book_id': self.book.id})
+
+class BookDetailsView(DetailView, FormMixin):
+    model = Book
+    template_name = 'book_details.html'
+    context_object_name = 'book'
+    form_class = RatingForm
+    pk_url_kwarg = 'book_id'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        user = self.request.user
+        book = self.get_object()
+        rating = book.ratings.all().filter(user=user)
+        if rating:
+            rating = rating[0]
+
+        context['book'] = book
+        context['form'] = RatingForm()
+        context['rating'] = rating
+        context['reviews'] = book.ratings.all().exclude(review='').exclude(user=user)
+        context['reviews_count'] = book.ratings.all().exclude(review='').exclude(user=user).count()
+        context['reader'] = book.is_reader(user)
+        context['numberOfRatings'] = book.ratings.all().count()
+        return context
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books.html'
+    form_class = BooksSortForm()
+    paginate_by = settings.BOOKS_PER_PAGE
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        """Retrieves the club_id from url and stores it in self for later use."""
+        self.club_id = kwargs.get('club_id')
+        self.user_id = kwargs.get('user_id')
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        books_queryset = Book.objects.all()
+        general = True
+
+        if self.club_id:
+            books_queryset = Club.objects.get(id=self.club_id).books.all()
+            general = False
+        if self.user_id:
+            books_queryset = User.objects.get(id=self.user_id).books.all()
+            general = False
+
+        form = BooksSortForm(self.request.GET or None)
+        sort = ""
+        if form.is_valid():
+            sort = form.cleaned_data.get('sort')
+            sort_helper = SortHelper(sort, books_queryset)
+            books_queryset = sort_helper.sort_books()
+
+        books_pg = Paginator(books_queryset, settings.BOOKS_PER_PAGE)
+        page_number = self.request.GET.get('page')
+        books = books_pg.get_page(page_number)
+        context['books'] = books
+        context['general'] = general
+        context['form'] = form
+        context['count'] = books_queryset.count()
+        return context
+
+# @login_required
+# def books_list(request, club_id=None, user_id=None):
+#     books_queryset = Book.objects.all()
+#     general = True
+#     if club_id:
+#         books_queryset = Club.objects.get(id=club_id).books.all()
+#         general = False
+#     if user_id:
+#         books_queryset = User.objects.get(id=user_id).books.all()
+#         general = False
+
+#     form = BooksSortForm(request.GET or None)
+#     sort = ""
+
+#     if form.is_valid():
+#         sort = form.cleaned_data.get('sort')
+#         sort_helper = SortHelper(sort, books_queryset)
+#         books_queryset = sort_helper.sort_books()
+
+#     count = books_queryset.count()
+#     books_pg = Paginator(books_queryset, settings.BOOKS_PER_PAGE)
+#     page_number = request.GET.get('page')
+#     books = books_pg.get_page(page_number)
+#     return render(request, 'books.html', {'books': books, 'general': general, 'count': count, 'form': form})
+
+
+@login_required
+def add_book_to_list(request, book_id):
+    book = get_object_or_404(Book.objects, id=book_id)
+    user = request.user
+    if book.is_reader(user):
+        book.remove_reader(user)
+        messages.add_message(request, messages.SUCCESS, "Book Removed!")
+    else:
+        book.add_reader(user)
+        request.user.add_book_to_all_books(book)
+        create_event('U', 'B', Event.EventType.ADD, user=user, book=book)
+        messages.add_message(request, messages.SUCCESS, "Book Added!")
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', reverse('home')))
+
+class AddReviewView(FormView):
+    template_name = 'book_details.html'
+    pk_url_kwarg = 'book_id'
+    context_object_name = 'book'
+    form_class = RatingForm
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        """Retrieves the book_id from url and stores it in self for later use."""
+        self.book_id = kwargs.get('book_id')
+        return super().get(self, request, *args, **kwargs)
+
+    def form_valid(self, form):
+        self.reviewed_book = get_object_or_404(Book.objects, id=self.kwargs['book_id'])
+        self.review_user = self.request.user
+        if self.reviewed_book.ratings.all().filter(user=self.review_user).exists():
+            return HttpResponseForbidden()
+
+        form.instance.user = self.review_user
+        form.instance.book = self.reviewed_book
+        form.save(self.review_user, self.reviewed_book)
+        self.review_user.add_book_to_all_books(self.reviewed_book)
+
+        create_event('U', 'B', Event.EventType.REVIEW, user=self.review_user, book=self.reviewed_book)
+        messages.add_message(self.request, messages.SUCCESS, 'you successfully submitted the review.')
+
+        self.reviewed_book.calculate_average_rating()
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR,
+                         "Review cannot be over 250 characters.")
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('book_details', kwargs = {'book_id': self.kwargs['book_id']})
+
+@login_required
+def add_review(request, book_id):
+    reviewed_book = get_object_or_404(Book.objects, id=book_id)
+    review_user = request.user
+    if reviewed_book.ratings.all().filter(user=review_user).exists():
+        return HttpResponseForbidden()
+
+    if request.method == 'POST':
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            form.instance.user = review_user
+            form.instance.book = reviewed_book
+            form.save(review_user, reviewed_book)
+            review_user.add_book_to_all_books(reviewed_book)
+            create_event('U', 'B', Event.EventType.REVIEW, user=review_user, book=reviewed_book)
+            messages.add_message(request, messages.SUCCESS, "you successfully submitted the review.")
+
+            reviewed_book.calculate_average_rating()
+
+            return redirect('book_details', book_id=reviewed_book.id)
+
+    messages.add_message(request, messages.ERROR,
+                         "Review cannot be over 250 characters.")
+    return render(request, 'book_details.html', {'book': reviewed_book})
+
+class EditReviewView(UpdateView):
+    """View to edit the user's review."""
+
+    model = Rating
+    template_name = 'edit_review.html'
+    pk_url_kwarg = 'review_id'
+    form_class = EditRatingForm
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['review'] = self.get_object()
+        return kwargs
+
+    def get(self, request, *args, **kwargs):
+        """Retrieves the review_id from url and stores it in self for later use."""
+        self.review_id = kwargs.get('review_id')
+        self.review = get_object_or_404(Rating.objects, id=self.review_id)
+        if self.request.user != self.review.user:
+            raise Http404
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['review_id'] = self.get_object().id
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        messages.add_message(self.request, messages.SUCCESS, "Successfully updated your review!")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR,
+                         "Review cannot be over 250 characters.")
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse('book_details', kwargs = {'book_id': self.get_object().book.id})
+
+
+# @login_required
+# def edit_review(request, review_id):
+#     review = get_object_or_404(Rating.objects, id=review_id)
+#     review_user = request.user
+#     if review_user == review.user:
+#         if request.method == "POST":
+#             form = EditRatingForm(data = request.POST, instance=review)
+#             if form.is_valid():
+#                 form.save(review_user, review.book)
+#                 messages.add_message(request, messages.SUCCESS, "Successfully updated your review!")
+#                 return redirect('book_details', book_id= review.book.id)
+#             messages.add_message(request, messages.ERROR, "Review cannot be over 250 characters!")
+#         else:
+#             form = EditRatingForm(instance = review)
+
+#         return render(request, 'edit_review.html', {'form':form , 'review_id':review.id })
+
+#     else:
+#         return render(request, '404_page.html', status=404)
+
+
+
+
+
+#########################################
+
+class AddBookView(FormView):
+    form_class = BookForm
     template_name = 'add_book.html' 
     
     @method_decorator(login_required)
@@ -134,60 +396,70 @@ def add_review(request, book_id):
                          "Review cannot be over 250 characters.")
     return render(request, 'book_details.html', {'book': reviewed_book})
 
-class EditReviewView(FormView):
+
+class EditReviewView(UpdateView):
+    """View to edit the user's review."""
+
     model = Rating
-    template_name = 'edit_review.html' 
+    template_name = 'edit_review.html'
     pk_url_kwarg = 'review_id'
     form_class = EditRatingForm
-
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-    
-    # def get(self, request, *args, **kwargs):
-    #     """Retrieves the club_id from url and stores it in self for later use."""
-    #     self.review_id = kwargs.get('review_id')  
-    #     return super().get(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['review'] = self.get_object()
+        return kwargs
+
+    def get(self, request, *args, **kwargs):
+        """Retrieves the review_id from url and stores it in self for later use."""
+        self.review_id = kwargs.get('review_id')
+        self.review = get_object_or_404(Rating.objects, id=self.review_id)
+        if self.request.user != self.review.user:
+            raise Http404
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['review_id'] = self.get_object().id
+        return context
 
     def form_valid(self, form):
-        review_user = self.request.user
-        review = get_object_or_404(Rating.objects, id=self.review_id)
-        if review_user != review.user:
-            raise Http404
-
-        form.save(review_user, review.book)
+        form.save()
         messages.add_message(self.request, messages.SUCCESS, "Successfully updated your review!")
+        return super().form_valid(form)
 
     def form_invalid(self, form):
         messages.add_message(self.request, messages.ERROR,
                          "Review cannot be over 250 characters.")
         return super().form_invalid(form)
 
-    def get_success_url(self): 
-        return reverse_lazy('book_details', kwargs = {'book_id': self.review.book.id})
-    
+    def get_success_url(self):
+        return reverse('book_details', kwargs = {'book_id': self.get_object().book.id})
 
 
-@login_required
-def edit_review(request, review_id):
-    review = get_object_or_404(Rating.objects, id=review_id)
-    review_user = request.user
-    if review_user == review.user:
-        if request.method == "POST":
-            form = EditRatingForm(data = request.POST, instance=review)
-            if form.is_valid():
-                form.save(review_user, review.book)
-                messages.add_message(request, messages.SUCCESS, "Successfully updated your review!")
-                return redirect('book_details', book_id= review.book.id)
-            messages.add_message(request, messages.ERROR, "Review cannot be over 250 characters!")
-        else:
-            form = EditRatingForm(instance = review)
-        
-        return render(request, 'edit_review.html', {'form':form , 'review_id':review.id })
+# @login_required
+# def edit_review(request, review_id):
+#     review = get_object_or_404(Rating.objects, id=review_id)
+#     review_user = request.user
+#     if review_user == review.user:
+#         if request.method == "POST":
+#             form = EditRatingForm(data = request.POST, instance=review)
+#             if form.is_valid():
+#                 form.save(review_user, review.book)
+#                 messages.add_message(request, messages.SUCCESS, "Successfully updated your review!")
+#                 return redirect('book_details', book_id= review.book.id)
+#             messages.add_message(request, messages.ERROR, "Review cannot be over 250 characters!")
+#         else:
+#             form = EditRatingForm(instance = review)
 
-    else:
-        return render(request, '404_page.html', status=404)
+#         return render(request, 'edit_review.html', {'form':form , 'review_id':review.id })
+
+#     else:
+#         return render(request, '404_page.html', status=404)
 
 
 class BookListView(ListView):
--- bookclub/views/user_views.py
+++ bookclub/views/user_views.py
@@ -11,153 +11,180 @@ from django.views.generic.base import TemplateView
 from django.views.generic import DetailView
 from system import settings
 
-@login_required
-def home(request):
-    def events_created_at(event):
+class HomeView(TemplateView):
+
+    template_name = 'home.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def events_created_at(self, event):
         return event.created_at
 
-    current_user = request.user
-    authors = list(current_user.followees.all()) + [current_user]
-    clubs = list(User.objects.get(id=current_user.id).clubs.all())
-    user_events = []
-    club_events = []
-    for author in authors:
-        user_events += list(Event.objects.filter(user=author))
-    final_user_events = user_events
-    final_user_events.sort(reverse=True, key=events_created_at)
-    first_twentyFive = final_user_events[0:25]
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        current_user = self.request.user
 
-    for club in clubs:
-        club_events += list(Event.objects.filter(club=club))
+        authors = list(current_user.followees.all()) + [current_user]
+        clubs = list(User.objects.get(id=current_user.id).clubs.all())
+        user_events = []
+        club_events = []
+        for author in authors:
+            user_events += list(Event.objects.filter(user=author))
 
-    final_club_events = club_events
-    final_club_events.sort(reverse=True, key=events_created_at)
-    first_ten = final_club_events[0:10]
+        final_user_events = user_events
+        final_user_events.sort(reverse=True, key=self.events_created_at)
+        context['user_events'] = final_user_events[0:25]
 
-    club_events_length = len(first_ten)
+        for club in clubs:
+            club_events += list(Event.objects.filter(club=club))
 
-    already_selected_books = current_user.books.all()
-    my_books = Book.objects.all().exclude(id__in=already_selected_books)
-    top_rated_books = my_books.order_by('-average_rating','-readers_count')[:3]
+        final_club_events = club_events
+        final_club_events.sort(reverse=True, key=self.events_created_at)
+        first_ten = final_club_events[0:10]
+        context['club_events'] = first_ten
+        context['club_events_length'] = len(first_ten)
 
-    return render(request, 'home.html', {'user': current_user, 'user_events': first_twentyFive, 'club_events': first_ten, 'club_events_length': club_events_length, 'books':top_rated_books})
+        already_selected_books = current_user.books.all()
+        my_books = Book.objects.all().exclude(id__in=already_selected_books)
+        context['books'] = my_books.order_by('-average_rating','-readers_count')[:3]
+
+        context['user'] = current_user
+        return context
+
+# @login_required
+# def home(request):
+#     def events_created_at(event):
+#         return event.created_at
+
+#     current_user = request.user
+#     authors = list(current_user.followees.all()) + [current_user]
+#     clubs = list(User.objects.get(id=current_user.id).clubs.all())
+#     user_events = []
+#     club_events = []
+#     for author in authors:
+#         user_events += list(Event.objects.filter(user=author))
+#     final_user_events = user_events
+#     final_user_events.sort(reverse=True, key=events_created_at)
+#     first_twentyFive = final_user_events[0:25]
+
+#     for club in clubs:
+#         club_events += list(Event.objects.filter(club=club))
+
+#     final_club_events = club_events
+#     final_club_events.sort(reverse=True, key=events_created_at)
+#     first_ten = final_club_events[0:10]
+
+#     club_events_length = len(first_ten)
+
+#     already_selected_books = current_user.books.all()
+#     my_books = Book.objects.all().exclude(id__in=already_selected_books)
+#     top_rated_books = my_books.order_by('-average_rating','-readers_count')[:3]
+
+#     return render(request, 'home.html', {'user': current_user, 'user_events': first_twentyFive, 'club_events': first_ten, 'club_events_length': club_events_length, 'books':top_rated_books})
 
 
+class ProfilePageView(DetailView):
+    model = User
+    pk_url_kwarg = 'review_id'
+    # paginate_by = settings.BOOKS_PER_PAGE
 
-@login_required
-def show_profile_page(request, user_id=None, is_clubs=False):
-    user = get_object_or_404(User.objects, id=request.user.id)
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
-    if user_id == request.user.id:
-        return redirect('profile')
+    def get(self, request, *args, **kwargs):
+        """Retrieves the user_id from url and stores it in self for later use."""
+        self.user_id = kwargs.get('user_id')
+        self.is_clubs = kwargs.get('is_clubs')
+        return super().get(request, *args, **kwargs)
 
-    if user_id:
-        user = get_object_or_404(User.objects, id=user_id)
-        
-
-    following = request.user.is_following(user)
-    followable = (request.user != user)
-
-    items = ""
-    items_count = 0
-
-    if user_id is not None:
-        books_queryset = User.objects.get(id=user_id).books.all()
-        books_count = books_queryset.count()
-        books_pg = Paginator(books_queryset, settings.BOOKS_PER_PAGE)
-        page_number = request.GET.get('page')
-        books = books_pg.get_page(page_number)
-        items = books
-        items_count = books_count
-
-        return render(request, 'profile_page.html', {'current_user': request.user, 'user': user, 'following': following, 'followable': followable, 'items': items, 'items_count': items_count, 'is_clubs': is_clubs})
-       
-    return render(request, 'profile_page.html', {'current_user': request.user, 'user': user, 'following': following, 'followable': followable, })
-
-@login_required
-def add_book_to_list(request, book_id):
-    book = get_object_or_404(Book.objects, id=book_id)
-    user = request.user
-    if book.is_reader(user):
-        book.remove_reader(user)
-        messages.add_message(request, messages.SUCCESS, "Book Removed!")
-    else:
-        book.add_reader(user)
-        create_event('U', 'B', Event.EventType.ADD, user=user, book=book)
-        messages.add_message(request, messages.SUCCESS, "Book Added!")
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER', reverse('home')))
-
-class SearchPageView(TemplateView):
-    
-    template_name = 'search_page.html'
-    paginate_by = settings.MEMBERS_PER_PAGE
-    
-    def get(self, *args, **kwargs):
-        self.searched = self.request.GET.get('searched')
-        self.category = self.request.GET.get('category')
-        return super().get(*args, **kwargs)
-    
-    
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, request, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        label = self.category
-
-        search_page_results = get_list_of_objects(
-            searched=self.searched, label=label)
-        self.category = search_page_results["category"]
-        filtered_list = search_page_results["filtered_list"]
-        
-        sortForm = ""
-        if(self.category == "Clubs"):
-            sortForm = NameAndDateSortForm(self.request.GET or None)
+        if self.user_id:
+            user = get_object_or_404(User.objects, id=self.user_id)
         else:
-            sortForm = NameSortForm(self.request.GET or None)
+            user = get_object_or_404(User.objects, id=request.user.id)
 
-        context['searched'] = self.searched
-        context['category'] = self.category
-        context['label'] = label
-        context['filtered_list'] = filtered_list
-        context['form'] = sortForm
-        context['current_user'] = self.request.user
+        if self.user_id == request.user.id:
+            return redirect('profile')
+
+        context['current_user'] = request.user
+        context['user'] = user
+        context['following'] = request.user.is_following(user)
+        context['followable'] = (request.user != user)
+
+        if self.user_id:
+            books_queryset = User.objects.get(id=self.user_id).books.all()
+            books_count = books_queryset.count()
+            books_pg = Paginator(books_queryset, settings.BOOKS_PER_PAGE)
+            page_number = request.GET.get('page')
+            books = books_pg.get_page(page_number)
+            context['items'] = books
+            context['items_count'] = books_count
         return context
 
 
-@login_required
-def search_page(request):
-    if request.method == 'GET':
-        searched = request.GET.get('searched')
-        category = request.GET.get('category')
-        label = category
+class InitialGenresView(TemplateView):
+    template_name = 'initial_genres.html'
 
-        # method in helpers to return a dictionary with a list of users, clubs or books searched
-        search_page_results = get_list_of_objects(
-            searched=searched, label=label)
-        category = search_page_results["category"]
-        filtered_list = search_page_results["filtered_list"]
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
-        sortForm = ""
-        if(category == "Clubs"):
-            sortForm = NameAndDateSortForm(request.GET or None)
-        else:
-            sortForm = NameSortForm(request.GET or None)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        genres = getGenres()
+        context['genres'] = sorted(genres, reverse=True, key=genres.get)[0:40]
+        return context
 
-        pg = Paginator(filtered_list, settings.MEMBERS_PER_PAGE)
-        page_number = request.GET.get('page')
-        filtered_list = pg.get_page(page_number)
-        current_user=request.user
-        return render(request, 'search_page.html', {'searched': searched, 'category': category, 'label': label, "filtered_list": filtered_list, "form": sortForm, "current_user":current_user})
 
-    else:
-        return render(request, 'search_page.html', {})
+# @login_required
+# def initial_genres(request):
+#     genres = getGenres()
+#     sorted_genres = sorted(genres, reverse=True, key=genres.get)[0:40]
 
-#wait for rahaf's ver
-@login_required
-def initial_book_list(request):
-    current_user = request.user
-    already_selected_books = current_user.books.all()
-    my_books = Book.objects.all().exclude(id__in=already_selected_books)
-    list_length = len(current_user.books.all())
-    sorted_books = my_books.order_by('-average_rating','-readers_count')[:8]
-    return render(request, 'initial_book_list.html', {'my_books':sorted_books , 'user':current_user , 'list_length':list_length })
+#     return render(request, 'initial_genres.html', {'genres':sorted_genres})
 
+class InitialBookListView(TemplateView):
+    template_name = 'initial_book_list.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_user = self.request.user
+        already_selected_books = current_user.books.all()
+        my_books = Book.objects.all().exclude(id__in=already_selected_books)
+
+        genres = self.request.GET.getlist('genre')
+        if genres:
+            for genre in genres:
+                my_books = my_books.filter(genre__contains=genre)
+
+        sorted_books = my_books.order_by('-average_rating','-readers_count')[:8]
+
+        context['my_books'] = sorted_books
+        context['list_length'] = len(current_user.books.all())
+        context['genres'] = genres
+        return context
+
+
+# @login_required
+# def initial_book_list(request):
+#     current_user = request.user
+#     already_selected_books = current_user.books.all()
+#     my_books = Book.objects.all().exclude(id__in=already_selected_books)
+
+#     genres = request.GET.getlist('genre')
+#     if genres:
+#         for genre in genres:
+#             my_books = my_books.filter(genre__contains=genre)
+
+#     sorted_books = my_books.order_by('-average_rating','-readers_count')[:8]
+
+#     list_length = len(current_user.books.all())
+#     return render(request, 'initial_book_list.html', {'my_books':sorted_books , 'list_length':list_length, 'genres':genres})
